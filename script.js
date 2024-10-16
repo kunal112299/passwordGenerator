dfcc54ff4699567a288446d0327f9d6d97682b22
kunal112299 <kunal11229900@gmail.com>
2024-10-16 13:28:50 +0530

    Category Added

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 let charLength = document.getElementById('number');
 let randomPasswordGenerated = document.getElementById('generated-password')
-let submitBtn = document.getElementById('submit') 
+let submitBtn = document.getElementById('submit');
+let alpha = document.getElementById('alpha');
+let num = document.getElementById('num');
+let sym = document.getElementById('symbol');
 
 let result = '';
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+const numeric = '0123456789';
+const symbol = '!@#$%&*'
+
+let characters = '' ;
+
+
+function options() {
+
+    characters = '';
+    if(alpha.checked){
+        characters += alphabet
+    }
+    if(num.checked){
+        characters +=numeric;
+    }
+    if(num.checked){
+        characters+=symbol
+    }
+    return characters;
+}
 
 function randomPassword (){
+    options();
     const length = charLength.value;
     
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&*'
+    
 
     for(let i = 0; i<length; i++){
         const randomInt = Math.floor(Math.random()*characters.length);
